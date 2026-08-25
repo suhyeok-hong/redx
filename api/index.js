47dc2172c9afdff3730e2db5ce4a5ce9a638f47d
suhyeok-hong <redx@daum.net>
2026-08-26 01:11:08 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
 export default async function handler(req, res) {
-  const target = req.query.url;
-  if (!target) return res.status(400).send('url parameter missing');
+  const targetUrl = req.query.url;
+  if (!targetUrl) {
+    return res.status(400).send('url 파라미터가 없습니다. ?url= 을 붙여주세요');
+  }
   try {
-    const r = await fetch(target);
-    const text = await r.text();
-    res.setHeader('Content-Type', 'text/html');
-    res.status(200).send(text);
+    const response = await fetch(targetUrl);
+    let html = await response.text();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    return res.status(200).send(html);
   } catch (e) {
-    res.status(500).send(e.message);
+    return res.status(500).send('프록시 에러: ' + e.message);
   }
 }
