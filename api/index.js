cf749fe91f3b4af98ab2f3c47ce742ee11a4a77b
suhyeok-hong <redx@daum.net>
2026-08-26 01:29:26 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,36 +2,21 @@ export const config = { api: { bodyParser: false } };
 
 export default async function handler(req, res) {
   const HOST = "http://redx.dothome.co.kr";
+  let path = req.url.replace(/^\/api/, '');
+  if (path === '') path = '/';
 
-  // 강제로 무조건 /visit/ 붙이기
-  let targetUrl = "";
-  const originalUrl = req.url; // /visit_pass.php?code=xxx 또는 /apply.php
-
-  if (req.query.url) {
-    targetUrl = req.query.url;
-  } else {
-    // 모든 요청을 /visit/ 안으로 강제 이동
-    let file = originalUrl;
-    if (file.startsWith('/api')) file = file.replace('/api','');
-
-    // file = /visit_pass.php?code=xxx
-    // file에서? 앞부분만 뽑기
-    let fileName = file.split('?')[0];
-    let query = file.includes('?')? '?' + file.split('?')[1] : '';
-
-    if (fileName === '' || fileName === '/') {
-      targetUrl = HOST + '/visit/' + query;
-    } else if (fileName.includes('visit_pass.php')) {
-      targetUrl = HOST + '/visit/visit_pass.php' + query;
-    } else if (fileName.includes('apply.php')) {
-      targetUrl = HOST + '/visit/apply.php' + query;
-    } else if (fileName.startsWith('/visit/')) {
-      targetUrl = HOST + fileName + query;
-    } else {
-      // 그 외 모든 파일은 /visit/ 아래에 있다고 가정
-      targetUrl = HOST + '/visit' + fileName + query;
-    }
+  // 핵심: 무조건 visit 폴더로 보내기
+  // /visit_pass.php -> /visit/visit_pass.php
+  // /visit_pass2.php -> /visit/visit_pass2.php
+  // /apply.php -> /visit/apply.php
+  if (!path.startsWith('/visit/') && !path.startsWith('/visit?')) {
+    if (path.startsWith('?')) path = '/visit/' + path;
+    else if (path === '/') path = '/visit/';
+    else path = '/visit' + (path.startsWith('/')? path : '/' + path);
   }
+
+  // ?url= 로 직접 지정되면 그걸 우선 사용
+  const targetUrl = req.query.url || (HOST + path);
 
   let body;
   if (req.method === 'POST') {
